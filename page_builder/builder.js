8fa711b6c932acba9b4863e4905c30c11d6e59e1
Max Smith <maxsmith@Maxs-MacBook-Pro.local>
2023-03-10 16:47:37 +0000

    page builder

---
+++ page_builder/builder.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+// CODE FOR TAGS
+const tagInput = document.getElementById("tag-input");
+const tagContainer = document.getElementById("tag-container");
+
+let tags = [];
+
+tagInput.addEventListener("keydown", function (event) {
+if (event.code === "Enter") {
+    event.preventDefault();
+
+    const value = tagInput.value.trim();
+    if (value === "") return;
+
+    const tag = createTag(value);
+    tagContainer.appendChild(tag);
+
+    tags.push(value);
+    tagInput.value = "";
+}
+});
+
+function createTag(value) {
+const tag = document.createElement("div");
+tag.classList.add("tag");
+tag.textContent = ("#" + value);
+
+tag.addEventListener("click", function () {
+    tagContainer.removeChild(tag);
+    tags = tags.filter((tagValue) => tagValue !== value);
+});
+
+return tag;
+}
+
+// OTHER CODE
+
+const inputText = document.getElementById('input-text');
+
+inputText.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === 'r') {
+    event.preventDefault();
+    const selectedText = inputText.value.substring(inputText.selectionStart, inputText.selectionEnd);
+    const ref = prompt("Enter reference:");
+    const fullRef = prompt("Enter full reference:");
+    const link = prompt("Enter link:");
+    const linkHTML = `<a reference="${ref}" full_reference="${fullRef}" id="reference" href="${link}">`;
+    inputText.setRangeText(linkHTML, inputText.selectionStart, inputText.selectionEnd, "end");
+  }
+});
+
+document.getElementById('build-page').addEventListener('click', function() {
+    var title = (document.getElementById('title')).value;
+    var desc = (document.getElementById('description')).value;
+    var date = (document.getElementById('date')).value;
+    var content = (document.getElementById('input-text')).value;
+
+    var toWriteDate = `<p id="date">${date}</p>`;
+    var toWriteTitle = `<h1 id="title">${title}</h1>`;
+    var toWriteDesc = `<h2 id="description">${desc}</h2><div id="title-tags">`;
+
+    for (let i=0; i < tags.length; i++){
+        let tagName = tags[i];
+        let formattedTag = `<a href="search-results.html?post=${tag}" class="tag">#${tag}</a>`;
+        toWriteDesc += formattedTag;
+        tags[i] = formattedTag;
+    };
+
+    var toWriteMain = `</div><p id="main-text-body">${content}<div id="follower"></div></p>`;
+
+    var finalToWrite = toWriteDate + toWriteTitle + toWriteDesc + toWriteMain;
+
+    console.log(finalToWrite)
+    
+
+
+
+})
+
+
+});
